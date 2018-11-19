a7ebd6a150176f8b93bcbf31163e8aa13a4ebadf
Ashley  Zaki <ahnahn.un@gmail.com>
2018-11-19 05:30:51 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,15 @@
+require 'pry'
 def reformat_languages(languages)
-  # your code here
+  reformatted = {}
+  languages.each do |type, hash1|
+    hash1.each do |name, hash2|
+      if !reformatted[name]
+        reformatted[name] = {:type => hash2[:type]}
+        reformatted[name][:style] = [type]
+      else
+        reformatted[name][:style] << type
+      end
+    end
+  end
+  return reformatted
 end
